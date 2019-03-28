a8f9ae5519191edc6a9d2340163ffb4d82ae77c4
Jasmeet singh <jasmeets58@yahoo.com>
2019-03-28 18:56:13 +0530

    Add files via upload

---
+++ sentimental_movie.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 23 20:30:46 2019
+
+@author: jasjag
+"""
+
+import sentiment_mod as s
+print(s.sentiment("This movie was great! i loved it."))
+print(s.sentiment("movie was wierd. i did not understand the suspense."))
